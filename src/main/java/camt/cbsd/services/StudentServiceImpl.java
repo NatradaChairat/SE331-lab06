1200f065dbd58340640895a33e7b85bcd4b0d208
Natrada Chairat <natrada_chairat@cmu.ac.th>
2017-09-21 22:30:09 +0700

    1.1 change root of image result: image is not show

--- src/main/java/camt/cbsd/services/StudentServiceImpl.java
+++ src/main/java/camt/cbsd/services/StudentServiceImpl.java
@@ -11,10 +11,11 @@ import java.util.List;
 @Profile("firstDataSource")
 @Service
 public class StudentServiceImpl implements StudentService {
+    String imageBaseUrl = "http://localhost:300/image/";
     public List<Student> getStudents(){
         List<Student> students = new ArrayList<>();
 
-        Student student = new Student(1,"SE-001","Mitsuha","Miyamizu",
+        /*Student student = new Student(1,"SE-001","Mitsuha","Miyamizu",
                 2.15,"images/mitsuha.gif",true,0,
                 "The most beloved one");
         students.add(student);
@@ -24,6 +25,17 @@ public class StudentServiceImpl implements StudentService {
         students.add(student);
         student = new Student(3,"SE-003","Jurgen","Kloop",
                 2.15,"images/Kloop.gif",true,2,
+                "The man for the Kop");*/
+        Student student = new Student(1,"SE-001","Mitsuha","Miyamizu",
+                2.15,imageBaseUrl+"mitsuha.gif",true,0,
+                "The most beloved one");
+        students.add(student);
+        student = new Student(2,"SE-002","Prayuth","The minister",
+                3.59,imageBaseUrl+"tu.jpg",false,15,
+                "The great man ever!!!!");
+        students.add(student);
+        student = new Student(3,"SE-003","Jurgen","Kloop",
+                2.15,imageBaseUrl+"Kloop.gif",true,2,
                 "The man for the Kop");
         students.add(student);
         return students;
